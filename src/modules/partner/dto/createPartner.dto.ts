28e025c72caf396a994565b81ef8263eb15ec71a
GabrielCamargoL <gabriel.cleite@outlook.com>
2023-03-21 12:02:13 -0300

    :memo: add partner fields in swagger

--- src/modules/partner/dto/createPartner.dto.ts
+++ src/modules/partner/dto/createPartner.dto.ts
@@ -9,43 +9,55 @@ import {
 
 import { PartnerClassification } from '../enums/PartnerClassification';
 import { PartnerStatus } from '../enums/PartnerStatus';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePartnerDTO {
   @IsNotEmpty({ message: 'O nome não pode ser vazio.' })
+  @ApiProperty()
   name: string;
 
   @IsEmail(undefined, { message: 'o e-mail informado é invalido' })
   @IsOptional()
+  @ApiProperty()
   email: string;
 
   @MaxLength(14, { message: 'O telefone deve conter no máximo 14 caracteres' })
   @MinLength(8, { message: 'O telefone deve conter pelo menos 8 caracteres' })
   @IsOptional()
+  @ApiProperty()
   phoneNumber: string;
 
   @MaxLength(8, { message: 'O CEP deve conter 8 caracteres' })
   @IsOptional()
+  @ApiProperty()
   zipCode: string;
 
   @IsOptional()
+  @ApiProperty()
   state: string;
 
   @IsOptional()
+  @ApiProperty()
   city: string;
 
   @IsOptional()
+  @ApiProperty()
   neighborhood: string;
 
   @IsOptional()
+  @ApiProperty()
   address: string;
 
   @IsOptional()
+  @ApiProperty()
   classification: PartnerClassification;
 
   @IsNotEmpty({ message: 'O status não pode ser vazio.' })
+  @ApiProperty()
   status: PartnerStatus;
 
   @IsNumber()
   @IsOptional()
+  @ApiProperty()
   memberNumber: number;
 }
--- src/modules/partner/dto/updatePartner.dto.ts
+++ src/modules/partner/dto/updatePartner.dto.ts
@@ -2,43 +2,55 @@ import { IsEmail, IsNumber, IsOptional, MaxLength, MinLength } from 'class-valid
 
 import { PartnerClassification } from '../enums/PartnerClassification';
 import { PartnerStatus } from '../enums/PartnerStatus';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdatePartnerDTO {
 	@IsOptional()
+	@ApiProperty()
 	name: string;
 
 	@IsEmail(undefined, { message: 'o e-mail informado é invalido' })
 	@IsOptional()
+	@ApiProperty()
 	email: string;
 
 	@MaxLength(14, { message: 'O telefone deve conter no máximo 14 caracteres' })
 	@MinLength(8, { message: 'O telefone deve conter pelo menos 8 caracteres' })
 	@IsOptional()
+	@ApiProperty()
 	phoneNumber: string;
 
 	@MaxLength(8, { message: 'O CEP deve conter 8 caracteres' })
 	@IsOptional()
+	@ApiProperty()
 	zipCode: string;
 
 	@IsOptional()
+	@ApiProperty()
 	state: string;
 
 	@IsOptional()
+	@ApiProperty()
 	city: string;
 
 	@IsOptional()
+	@ApiProperty()
 	neighborhood: string;
 
 	@IsOptional()
+	@ApiProperty()
 	address: string;
 
 	@IsOptional()
+	@ApiProperty()
 	classification: PartnerClassification;
 
 	@IsOptional()
+	@ApiProperty()
 	status: PartnerStatus;
 
 	@IsNumber()
 	@IsOptional()
+	@ApiProperty()
 	memberNumber: number;
 }

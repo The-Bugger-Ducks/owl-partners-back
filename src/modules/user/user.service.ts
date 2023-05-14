60e283bb6dd29a6a81c053351cd78ff5d14f6150
GabrielCamargoL <gabriel.cleite@outlook.com>
2023-05-14 18:33:51 -0300

    :bug: Fix update password

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,59 +6,70 @@ import { PrismaService } from "src/database/prisma/prisma.service";
 import { CreateUserDTO } from './dto/createUser.dto';
 import { UpdateUserDTO } from './dto/updateUser.dto';
 
+const saltOrRounds = 10;
+
 @Injectable()
 export class UserService {
-  constructor(private readonly prismaService: PrismaService) { }
+	constructor(private readonly prismaService: PrismaService) { }
 
-  findAll() {
-    return this.prismaService.user.findMany();
-  }
+	findAll() {
+		return this.prismaService.user.findMany();
+	}
 
-  async findById(id: string) {
-    return this.prismaService.user.findUnique({
-      where: {
-        id
-      }
-    });
-  }
+	async findById(id: string) {
+		return this.prismaService.user.findUnique({
+			where: {
+				id
+			}
+		});
+	}
 
-  async create(user: CreateUserDTO) {
-    const saltOrRounds = 10;
-    const hash = await bcrypt.hash(user.password, saltOrRounds);
-    user.password = hash;
+	async create(user: CreateUserDTO) {
+		const hash = await bcrypt.hash(user.password, saltOrRounds);
+		user.password = hash;
 
-    return this.prismaService.user.create({
-      data: user,
-    });
-  }
+		return this.prismaService.user.create({
+			data: user,
+		});
+	}
 
-  update(user: UpdateUserDTO, id: string) {
-    return this.prismaService.user.update({
-      data: user,
-      where: {
-        id,
-      },
-    });
-  }
+	async update(user: UpdateUserDTO, id: string) {
 
-  delete(id: string) {
-    return this.prismaService.user.delete({
-      where: {
-        id: id,
-      },
-    });
-  }
+		console.log(user.password)
 
-  userExists(email: string): boolean {
-    const userFound = this.prismaService.user.findUnique({ where: { email: email } });
-    return userFound === undefined;
-  }
+		if (user.password) {
+			const hash = await bcrypt.hash(user.password, saltOrRounds);
+			user.password = hash;
+		}
 
-  findUserByEmail(email: string) {
-    const userFound = this.prismaService.user.findUnique({ where: { email: email } });
+		const userUpdated = this.prismaService.user.update({
+			data: user,
+			where: {
+				id,
+			},
+		});
 
-    if (!userFound) throw new NotFoundException('Usuário não encontrado');
-    return userFound;
-  }
+		return userUpdated
+	}
+
+	delete(id: string) {
+		return this.prismaService.user.delete({
+			where: {
+				id: id,
+			},
+		});
+	}
+
+	userExists(email: string): boolean {
+		const userFound = this.prismaService.user.findUnique({ where: { email: email } });
+		return userFound === undefined;
+	}
+
+	findUserByEmail(email: string) {
+		const userFound = this.prismaService.user.findUnique({ where: { email: email } });
+
+		if (!userFound) throw new NotFoundException('Usuário não encontrado');
+		return userFound;
+	}
 
 }

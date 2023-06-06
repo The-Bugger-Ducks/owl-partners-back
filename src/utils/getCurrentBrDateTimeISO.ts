d04b8a0b82ac108d3d5139391ae445a8a3b0f58c
GabrielCamargoL <gabriel.cleite@outlook.com>
2023-06-05 23:37:50 -0300

    :bug: Trying fix comment datetime

--- src/utils/getCurrentBrDateTimeISO.ts
+++ src/utils/getCurrentBrDateTimeISO.ts
@@ -1,11 +1,13 @@
 export function getCurrentBrDateTimeISO(): string {
 	const now = new Date()
+	console.log("Hora UTC: ", now.getHours(), ":", now.getMinutes())
 	const offset = -180 // offset em minutos (-180 equivale a -3 horas, ou seja, horário de Brasília)
 	const localTimestamp = now.getTime() + offset * 60 * 1000 // adiciona o offset em milissegundos
 
 	const localDate = new Date(localTimestamp)
 	const formattedDate = localDate.toISOString()
+	console.log("Hora Formatada: ", localDate.getHours(), ":", localDate.getMinutes())
 
-	return formattedDate
+	return now.toISOString()
 }
 
